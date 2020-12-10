ad769cef06d180ad8977ae997745e3ee2950dd9d
jkasze <jkasze@126.com>
2020-12-10 22:32:51 +0800

    restore startup.cs

--- internetProgramming_TeemProject/Startup.cs
+++ internetProgramming_TeemProject/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,10 +27,6 @@ namespace internetProgramming_TeemProject
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "internetProgramming_TeemProject", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +35,6 @@ namespace internetProgramming_TeemProject
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "internetProgramming_TeemProject v1"));
             }
 
             app.UseStaticFiles();

4ff35eed4f46e843bf667ddab15882f9df3921f4
jkasze <jkasze@126.com>
2020-12-10 22:07:05 +0800

    5.0->3.1

--- internetProgramming_TeemProject/Startup.cs
+++ internetProgramming_TeemProject/Startup.cs
@@ -44,7 +44,6 @@ namespace internetProgramming_TeemProject
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "internetProgramming_TeemProject v1"));
             }
 
-            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseHttpsRedirection();

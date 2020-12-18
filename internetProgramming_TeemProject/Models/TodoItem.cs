49c983ba30e1bba7bfc2e0d40da6d0a54a21fb4b
duce73 <duce2692415564@gmail.com>
2020-12-18 10:16:28 +0800

    测试数据库上下文与模型

---
+++ internetProgramming_TeemProject/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace internetProgramming_TeemProject.Models
+{
+    public class TodoItem
+    {
+        public long Id { get;  set;  }
+
+        public string Name { get; set; }
+
+        public bool IsCompate { get; set; }
+    }
+}

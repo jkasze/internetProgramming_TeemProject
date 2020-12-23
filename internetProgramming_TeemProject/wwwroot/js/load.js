40a93238421692ed8dd3c6d9a95a932c02ebac09
duce73 <duce2692415564@gmail.com>
2020-12-23 15:47:22 +0800

    完善登录验证，初步完成注册页面

--- internetProgramming_TeemProject/wwwroot/js/load.js
+++ internetProgramming_TeemProject/wwwroot/js/load.js
@@ -2,6 +2,8 @@ const form = document.getElementById("form");
 const userid = document.getElementById("userid");
 const password = document.getElementById("password");
 const identity =  document.getElementsByName("identity");
+const button = document.getElementById("register");
+
 
 const uri = "";
 
@@ -9,6 +11,12 @@ const uri = "";
 form.addEventListener("submit",(Event)=>{
     var value;
     
+    //判断是否符合账号规定
+    if(check(userid,password) != true){
+        Event.preventDefault();
+        return;
+    }
+
     //获取选取的身份
     for(let i = 0;i < identity.length; i++){
         if(identity[i].checked == true){
@@ -34,9 +42,33 @@ form.addEventListener("submit",(Event)=>{
             window.location.href = value+".html?"+ "id="+userid.value;
         }
         else{
-            alert("该账号未注册！请先注册账号")
+            alert("账号或密码输入错误，请重试或注册!")
         }
     })
     .catch(error => console.error('Unable to check account', error));
     Event.preventDefault();
+})
+
+function check(id, pwd){
+    //账号为空
+    if(id.value == null || id.value == undefined || id.value == ""){
+        alert("请输入账号！");
+        return false;
+    }
+    //密码为空
+    if(pwd.value == null || pwd.value == undefined || pwd.value == ""){
+        alert("请输入密码!");
+        return false;
+    }
+    //账号不能为非数字字符
+    if(isNaN(Number(id.value))){
+        alert("账号应为数字！");
+        return false;
+    }
+    return true;
+}
+
+//跳转到注册页面
+button.addEventListener("click",(Event)=>{
+    window.location.href = "register.html";
 })

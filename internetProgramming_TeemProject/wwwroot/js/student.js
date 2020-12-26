432fa4de5de88080251eaaaea6abf0c333a29b88
miaodashi <64314333+miaodashi@users.noreply.github.com>
2020-12-26 16:29:13 +0800

    Add files via upload

--- internetProgramming_TeemProject/wwwroot/js/student.js
+++ internetProgramming_TeemProject/wwwroot/js/student.js
@@ -1,1 +1,12 @@
-﻿
+﻿function getFileContent(){
+    var reader = new FileReader();
+
+    var files = document.getElementById("fileInput").files;
+    reader.readAsDataURL(files[0]);	/*这里只有一个文件*/
+
+    /*当文件读取完成后*/
+    reader.onload=function(){
+        /*显示图片*/
+        document.getElementById("imgShower").src = reader.result;
+    }
+}
